a75dc3766817686b6e1c6d09fa91297a0b3be150
Daniil_Hrynyshyn <hrynyshyn-ee27@lll.kpi.ua>
2024-10-30 00:29:42 +0200

    Add documentation

--- canvas/accounts/admin.py
+++ canvas/accounts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import CustomUser
+
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ('id', 'email', 'username', 'first_name', 'last_name', 'is_active', 'is_staff')
