4e3d46102c8b249beedfe16d71a8bc836dea3635
trekfest <110569040+trekfest@users.noreply.github.com>
2024-11-14 17:34:48 +0200

    Delete UserProfilePhtotoSerializer.py

--- UserProfilePhtotoSerializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import SUerProfile
-
-
-class UserProfilePhotoSerializer(serializers.ModelSerializer):
-    class Meta:
-         model = UserProfile
-         fields = ['profile_photo']
